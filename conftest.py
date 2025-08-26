af769c6e84401039bc7d2c2e22997353af8a33ad
MashaChernova <mchernova@inobitec.com>
2025-08-26 22:06:10 +0300

    домашняя работа по оркестрации докеров. Тесты собираются в докер-образ tests3, а затем запускаются при запуске opencart. Для этого в папке OPENCART в командной строке добавить параметр BROWSER BROWSEW=chrome docker compose up -d

--- conftest.py
+++ conftest.py
@@ -6,17 +6,16 @@ from selenium.webdriver.chromium.options import ChromiumOptions
 from selenium.webdriver.chromium.service import ChromiumService
 from selenium.webdriver.firefox.options import Options as FFOptions
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-
-
+import logging
+import datetime
 
 
 def pytest_addoption(parser):
     parser.addoption("--browser", help="Browser to run tests", default="chrome")
     parser.addoption("--drivers", help="Driver storage", default=r"C:\Users\Mariya\Downloads\drivers")
     parser.addoption("--headless", action="store_true", help="Browser run headless")
-    parser.addoption(
-        "--base_url", help="Base application url", default="192.168.0.231:8181"
-    )
+    parser.addoption("--remote", help="selenoid", default="True")
+    parser.addoption("--base_url", help="Base application url", default="192.168.0.106:8081")
 
 
 @pytest.fixture(scope="session")
@@ -29,17 +28,58 @@ def browser(request):
     browser_name = request.config.getoption("--browser")
     drivers_storage = request.config.getoption("--drivers")
     headless = request.config.getoption("--headless")
+    remote = request.config.getoption("--remote")
+    log_level = request.config.getoption("--log_level", default='INFO')
+
+    logger = logging.getLogger(request.node.name)
+    file_handler = logging.FileHandler(f"logs/{request.node.name}.log")
+    file_handler.setFormatter(logging.Formatter('%(levelname)s %(message)s'))
+    logger.addHandler(file_handler)
+    logger.setLevel(level=log_level)
+    logger.info("===> Test started at %s" % datetime.datetime.now())
+
     if browser_name in ["ch", "chrome"]:
         options = ChromeOptions()
         if headless:
             options.add_argument("headless=new")
-        driver = webdriver.Chrome(options=options)
-        driver.maximize_window()
+        if remote == "False":
+            logger.info('local chrome')
+            driver = webdriver.Chrome(options=options)
+        else:
+            logger.info('selenoid chrome')
+            capabilities = {
+                "browserName": "chrome",
+                "browserVersion": "latest",
+                "selenoid:options": {
+                    "enableVideo": False
+                }
+            }
+            for key, value in capabilities.items():
+                options.set_capability(key, value)
+
+            driver = webdriver.Remote(
+                command_executor="http://192.168.0.106/wd/hub",
+                options=options)
+            driver.maximize_window()
     elif browser_name in ["ff","fox","firefox"]:
         options=FFOptions()
         if headless:
             options.add_argument("headless")
-        driver = webdriver.Firefox(options=options)
+        if remote == "False":
+            driver = webdriver.Firefox(options=options)
+        else:
+            capabilities = {
+                "browserName": "firefox",
+                "browserVersion": "latest",
+                "selenoid:options": {
+                    "enableVideo": False
+                }
+            }
+            for key, value in capabilities.items():
+                options.set_capability(key, value)
+            driver = webdriver.Remote(
+                command_executor="http://192.168.0.106/wd/hub",
+                options=options)
     elif browser_name in ["ya","yandex"]:
         options = ChromiumOptions()
         if headless:
@@ -47,14 +87,12 @@ def browser(request):
         options.binary_location = r"c:\Users\Mariya\AppData\Local\Yandex\YandexBrowser\Application\browser.exe"
         driver = webdriver.Chrome(options=options, service=ChromiumService(
             executable_path=fr"{drivers_storage}\yandexdriver.exe"))
-    yield driver
-    driver.quit()
 
-# @pytest.fixture(scope="session")
-# def start_opencart ():
-#     ssh = paramiko.SSHClient()
-#     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#     ssh.connect('192.168.0.231', 22, username='user', password='123456')
-#     ssh.exec_command("cd /home/user/Documents/OPENCART")
-#     ssh.exec_command("docker compose up -d")
-#     return 0
+    driver.log_level = log_level
+    driver.logger = logger
+    driver.test_name = request.node.name
+
+    logger.info("Browser %s started" % browser)
+    yield driver
+    logger.info("===> Test finished at %s" % datetime.datetime.now())
+    driver.quit()
